f7709fc711959ae8ecef9a877f81cc1f886765c6
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-05-18 22:41:53 +0300

    Operators in C#

--- HelloFromCSharp/Program.cs
+++ HelloFromCSharp/Program.cs
@@ -1,8 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Please enter your name: ");
-string name = Console.ReadLine();
+﻿// C# Sprint learning
 
-Console.WriteLine("Please enter your second name: ");
-string lastName = Console.ReadLine();
+double ratePerHour = 12.34;
+int numberOfHoursWorked = 165;
+int bonus = 100;
 
-Console.WriteLine("Hello " + name + " " + lastName);
+double currentMonthWage = ratePerHour * numberOfHoursWorked+bonus;
+Console.WriteLine(currentMonthWage);
+
+ratePerHour += 3; //ratePerHour = ratePerHour + 3;
+
+if(currentMonthWage > 2000)
+{
+	Console.WriteLine("Top paid employee");
+}
+
+int numberOfEmployees = 15;
+numberOfEmployees--;
+
+bool a;
+int b;
+
+Console.WriteLine();
+
+
+ 
