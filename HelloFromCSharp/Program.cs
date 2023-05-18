3bb015f8b13a02ca19618cd621d148212b78ee4b
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-05-18 23:35:47 +0300

    Converting between types

--- HelloFromCSharp/Program.cs
+++ HelloFromCSharp/Program.cs
@@ -1,21 +1,5 @@
-﻿DateTime hireDate = new DateTime(2023, 5, 22, 15, 30, 0);
-Console.WriteLine(hireDate);
+﻿int numberOfHoursWorked = 165;
+long conversion = numberOfHoursWorked; // works fine
 
-DateTime exitDate = new DateTime(2030, 10, 11);
-//DateTime invalidDate = new DateTime(2032, 15, 11);
-
-DateTime startDate = hireDate.AddDays(3);
-Console.WriteLine(startDate);
-
-DateTime currentDate = DateTime.Now;
-Console.WriteLine(currentDate);
-
-bool areWeInDst = currentDate.IsDaylightSavingTime();
-Console.WriteLine(areWeInDst);
-
-DateTime startHour = DateTime.Now;
-TimeSpan workTime = new TimeSpan(8, 35, 0);
-DateTime endHour = startHour.Add(workTime);
-
-Console.WriteLine("Start hour: " + startHour.ToLongDateString());
-Console.WriteLine("End hour: " + endHour.ToLongDateString());
+double d = 123456789.0;
+int x = (int) d;
