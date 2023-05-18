d7d95b4ff05a2108221d2ee53a44923c0e691597
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-05-18 21:36:21 +0300

    Add project files.

---
+++ HelloFromCSharp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please enter your name: ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Please enter your second name: ");
+string lastName = Console.ReadLine();
+
+Console.WriteLine("Hello " + name + " " + lastName);
