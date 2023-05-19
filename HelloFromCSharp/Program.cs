d9c001b3fdb51e34b594cf6a3d0ea2daa468ff03
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-05-19 10:35:29 +0300

    Flow of execution if-else if-else

--- HelloFromCSharp/Program.cs
+++ HelloFromCSharp/Program.cs
@@ -1,10 +1,31 @@
-﻿int age = 23;
-bool a = (age == 23);
-Console.WriteLine("23 : " + a);
+﻿Console.WriteLine("Enter the age of the new canditate: ");
+int age = int.Parse(Console.ReadLine());
 
-bool b = (age >= 18) && (age <= 65);
-Console.WriteLine("Age is between 18 and 65:" + b);
+if(age < 18)
+{
+	Console.WriteLine("Too young to apply");
+}
+else if(age > 65)
+{
+	Console.WriteLine("You need to rest old buddy! :D");
+}
+else
+{
+	Console.WriteLine("You can apply");
+}
 
-bool c = (age >= 18)  || (age <= 65);
-Console.WriteLine("Age1 is between 18 or age2 is 65:" + c);
+DateTime today = DateTime.Now;
+bool endOfMonthPaymentStarted = false;
 
+if(today.Date.Day == 20)
+{
+	Console.WriteLine("Please start end-of-month employee payments");
+}
+else if(today.Date.Day >= 25 && !endOfMonthPaymentStarted)
+{
+	Console.WriteLine("Payments will be late");
+}
+else
+{
+	Console.WriteLine("YOU DON'T NEED A JOB =))");
+}
