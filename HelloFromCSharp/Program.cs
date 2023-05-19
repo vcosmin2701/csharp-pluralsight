e8ca57dfd4de127aef72980030d91db22e3d9d69
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-05-19 11:22:36 +0300

    for loop

--- HelloFromCSharp/Program.cs
+++ HelloFromCSharp/Program.cs
@@ -1,52 +1,12 @@
-﻿Console.WriteLine("Choose the action you want to do: ");
-Console.WriteLine("1. Add employee");
-Console.WriteLine("2. Update employee");
-Console.WriteLine("3. Delete employee");
-Console.WriteLine("99. Exit application");
+﻿Console.WriteLine("Enter a value");
+int max = int.Parse(Console.ReadLine());
 
-string selectedAction = Console.ReadLine();
-while (selectedAction != "99")
+for(int i = 0; i < max; i++)
 {
-	switch (selectedAction)
+	if (i == 5)
 	{
-		case "1":
-			Console.WriteLine("Adding new employee....");
-			break;
-		case "2":
-			Console.WriteLine("Update employee..");
-			break;
-		case "3":
-			Console.WriteLine("Deleting employee...");
-			break;
-		default:
-			Console.WriteLine("Invalid input.");
-			break;
+		Console.WriteLine("Bingo! " + i + " was found!");
+		break;
 	}
-	Console.WriteLine("Choose the action you want to do: ");
-	Console.WriteLine("1. Add employee");
-	Console.WriteLine("2. Update employee");
-	Console.WriteLine("3. Delete employee");
-	Console.WriteLine("99. Exit application");
-	selectedAction = Console.ReadLine();
-}
-
-Console.WriteLine("Closing application");
-
-int i = 0;
-int j = 0;
-
-while(i < 10)
-{
-	while(j < 10)
-	{
-		Console.WriteLine("i: " + i + "    j: " + j);
-		j++;
-	}
-	j = 0;
-	i++;
-}
-
-while (true)
-{
-	Console.WriteLine(DateTime.Now);
+	Console.WriteLine(i);
 }
