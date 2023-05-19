2700068aa5511d5380dda72213a72b5b5eddf592
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-05-19 15:21:26 +0300

    Method overloading

--- HelloFromCSharp/Program.cs
+++ HelloFromCSharp/Program.cs
@@ -1,17 +1,20 @@
-﻿int amount = 1234;
-int months = 12;
+﻿using HelloFromCSharp;
 
-int yearlyWage = CalculateYearlyWage(amount, months);
+int amount = 1234;
+int months = 12;
+int bonus = 1000;
+
+int yearlyWage = Utilities.CalculateYearlyWage(amount, months, bonus);
+
 Console.WriteLine($"Yearly wage: {yearlyWage}");
 
-static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked)
-{
-	//Console.WriteLine($"Yearly wage: {monthlyWage * numberOfMonthsWorked}");
-	if(numberOfMonthsWorked == 12)
-	{
-		return monthlyWage * (numberOfMonthsWorked + 1);
-	}
-	
-	return monthlyWage * numberOfMonthsWorked;
 
-}
+double amountDouble = 1500.0;
+double monthsDouble = 12;
+double bonusDouble = 1000;
+
+double yearlyWageWithBonusDouble = Utilities.CalculateYearlyWage(amountDouble, monthsDouble, bonusDouble);
+
+
+
+
---
+++ HelloFromCSharp/Utilities.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloFromCSharp
+{
+	internal class Utilities
+	{
+		public static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked)
+		{
+			//Console.WriteLine($"Yearly wage: {monthlyWage * numberOfMonthsWorked}");
+			if (numberOfMonthsWorked == 12)
+			{
+				return monthlyWage * (numberOfMonthsWorked + 1);
+			}
+
+			return monthlyWage * numberOfMonthsWorked;
+
+		}
+
+		public static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked, int bonus)
+		{
+			Console.WriteLine($"The yearly wage is: {monthlyWage * numberOfMonthsWorked + bonus}");
+			return monthlyWage + numberOfMonthsWorked + bonus;
+		}
+
+		public static double CalculateYearlyWage(double monthlyWage, double numberOfMonthsWorked, double bonus)
+		{
+			Console.WriteLine($"The yearly wage is: {monthlyWage * numberOfMonthsWorked + bonus}");
+			return monthlyWage + numberOfMonthsWorked + bonus;
+		}
+	}
+}
